1ab49e86c1efedc5c823d9188cfe6df9ec9a1661
Walter Fritsche <wfritsche95@protonmail.com>
2024-06-25 11:06:45 -0500

    ran prettier

--- routes/users.ts
+++ routes/users.ts
@@ -4,8 +4,11 @@ import { isAuthed } from "../lib/middleware/authMiddleware";
 const router = express.Router();
 
 /* GET users listing. */
-router.get("/", [isAuthed, function (req: Request, res: Response, next: NextFunction) {
-  res.json({success: true, message: `Hello, ${req.session?.name}`})
-}]);
+router.get("/", [
+  isAuthed,
+  function (req: Request, res: Response, next: NextFunction) {
+    res.json({ success: true, message: `Hello, ${req.session?.name}` });
+  },
+]);
 
 export default router;
