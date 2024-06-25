575f42d21c1425dfc2637347ed2ba72817ce7d9f
Walter Fritsche <wfritsche95@protonmail.com>
2024-06-25 10:08:45 -0500

    a test endpoint to see logged in users name

--- routes/users.ts
+++ routes/users.ts
@@ -1,10 +1,11 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { isAuthed } from "../lib/middleware/authMiddleware";
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+router.get("/", [isAuthed, function (req: Request, res: Response, next: NextFunction) {
+  res.json({success: true, message: `Hello, ${req.session?.name}`})
+}]);
 
 export default router;
