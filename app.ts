f98f3392732fb71f14b99ccfa24ee97f84f0df0f
Walter Fritsche <wfritsche95@protonmail.com>
2024-12-12 11:05:11 -0600

    upgraded project and added socket.io

--- app.ts
+++ app.ts
@@ -16,6 +16,9 @@ import http from "http";
 import { ErrorWithStatus } from "./types/types";
 import cors from "cors";
 import helmet from "helmet";
+import { Server as SocketServer } from "socket.io";
+import { jwtVerify } from "jose";
+import { db } from "./lib/db";
 
 const app = express();
 
@@ -40,6 +43,54 @@ app.use("/", indexRouter);
 app.use("/user", userRouter);
 app.use("/chats", chatsRouter);
 const server = http.createServer(app);
+const io = new SocketServer(server, {
+  cors: {
+    origin: [
+      process.env.NODE_ENV === "production"
+        ? process.env.PROD_FRONTEND_ORIGIN!
+        : process.env.DEV_FRONTEND_ORIGIN!,
+    ],
+  },
+});
+
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token;
+  if (!token) {
+    const err = new Error("Please attach token");
+    next(err);
+  }
+  const secret = new TextEncoder().encode(process.env.JWT_SIGNING_KEY!);
+  jwtVerify(token, secret)
+    .then((payload) => {
+      socket.data.userId = payload.payload.sub;
+      next();
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+// websocket events
+io.on("connection", (socket) => {
+  console.log(`a user connected: ${socket.id}`);
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    console.log(`${socket.id} joined room: ${room}`);
+  });
+  socket.on("send-message", async (message, room) => {
+    if (room === "" || room === undefined) {
+      return;
+    }
+    const user = await db.query.users.findFirst({
+      where: (user, { eq }) => eq(user.id, socket.data.userId!),
+    });
+    socket.to(room).emit("receive-message", `${user?.name} ${message}`);
+  });
+  socket.on("disconnect", (reason) => {
+    console.log(`${socket.id} disconnected because: `, reason);
+  });
+});
+
 const port = parseInt(process.env.PORT || "3000");
 app.set("port", port);
 
